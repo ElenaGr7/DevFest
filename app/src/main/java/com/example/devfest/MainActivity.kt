11ffb3d092cf464179c33910771b0a725d0e129b
Elena Grishina <Lena_10@bk.ru>
2019-04-10 22:20:32 +0300

    v.1.1

--- app/src/main/java/com/example/devfest/MainActivity.kt
+++ app/src/main/java/com/example/devfest/MainActivity.kt
@@ -5,8 +5,11 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val ACTION_OPEN = "com.example.devfest.SHOW_AUTHOR"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         linkButton.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://devfest-spb.com/"))
             startActivity(browserIntent)
+        }
+
+        val openAuthor = findViewById<TextView>(R.id.speaker)
+        openAuthor.setOnClickListener {
+            val openAuthorIntent = Intent(ACTION_OPEN)
+                    startActivity(openAuthorIntent)
         }
     }
 }
---
+++ app/src/main/java/com/example/devfest/author.kt
@@ -0,0 +1,33 @@
+package com.example.devfest
+
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.TextView
+import android.widget.ImageView
+
+class Author : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_author)
+        val textDescriptionAuthor = findViewById<TextView>(R.id.descriptionAuthor)
+        textDescriptionAuthor.setMovementMethod(ScrollingMovementMethod())
+
+        val telegramButton = findViewById<ImageView>(R.id.telegram)
+        telegramButton.setOnClickListener {
+            val telegramIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://telegram.com/"))
+            startActivity(telegramIntent)
+        }
+
+        val twitterButton = findViewById<ImageView>(R.id.twitter)
+        twitterButton.setOnClickListener {
+            val twitterIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/"))
+            startActivity(twitterIntent)
+        }
+
+    }
+}
