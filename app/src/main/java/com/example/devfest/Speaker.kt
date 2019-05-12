3b1a3664dd61c54e3b2d1bf53d2949caf93cccc1
Elena Grishina <Lena_10@bk.ru>
2019-05-12 03:52:00 +0300

    v.1.2

--- app/src/main/java/com/example/devfest/Speaker.kt
+++ app/src/main/java/com/example/devfest/Speaker.kt
@@ -3,50 +3,5 @@ package com.example.devfest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-public class Speaker  {
-
-    private var name: String? = null
-    private var prof: String? = null
-    private var city: String? = null
-    private var photo: Int = 0
-
-    constructor(name: String, prof: String, city: String, photo: Int){
-
-        this.name = name
-        this.prof = prof
-        this.city = city
-        this.photo = photo
-    }
-
-    fun getName(): String? {
-        return this.name
-    }
-
-    fun setName(name: String) {
-        this.name = name
-    }
-
-    fun getProf(): String? {
-        return this.prof
-    }
-
-    fun setProf(prof: String) {
-        this.prof = prof
-    }
-
-    fun getCity(): String? {
-        return this.city
-    }
-
-    fun setCity(city: String) {
-        this.city = city
-    }
-
-    fun getPhoto(): Int {
-        return this.photo
-    }
-
-    fun setPhoto(photo: Int) {
-        this.photo = photo
-    }
+class Speaker(var name: String? = null,var prof: String? = null,var city: String? = null,var photo: Int = 0) {
 }
